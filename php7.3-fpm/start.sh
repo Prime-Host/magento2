6cf231a5b5f6f944e3cc6f2b105b3197d7851422
root <root@vmd38058.contaboserver.net>
2020-09-01 15:49:17 +0200

    new version

--- php7.3-fpm/start.sh
+++ php7.3-fpm/start.sh
@@ -15,5 +15,25 @@ if [ ! -f /home/$PRIMEHOST_USER ]; then
   echo "root:$PRIMEHOST_PASSWORD" | chpasswd
 fi
 
+# Enviroment Variables for cronjob and backup folder
+printenv > /etc/environment
+sed -i s,/root,/home/$PRIMEHOST_USER,g /etc/environment
+if [ ! -f /home/$PRIMEHOST_USER/backup ]; then
+    mkdir /home/$PRIMEHOST_USER/backup
+fi
+
+# insert cronjob
+if [ ! -f /var/spool/cron/crontabs/$PRIMEHOST_USER ]; then
+sudo -u $PRIMEHOST_USER bash << EOF
+crontab -l | { cat; echo "TZ=Europe/Berlin
+SHELL=/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+* * * * * perl -le 'sleep rand 40' && php /usr/share/nginx/www/bin/magento cron:run | grep -v 'Ran jobs by schedule' >> /usr/share/nginx/www/var/log/magento.cron.log && php /usr/share/nginx/www/update/cron.php >> /usr/share/nginx/www/var/log/update.cron.log && php /usr/share/nginx/www/bin/magento setup:cron:run >> /usr/share/nginx/www/var/log/setup.cron.log
+0 3 * * * mysqldump -u"root" -p"$PRIMEHOST_PASSWORD" -h"$PRIMEHOST_DOMAIN"-db magento2 > /home/$PRIMEHOST_USER/backup/dump.sql"; } | crontab -
+EOF
+fi
+
+unset PRIMEHOST_USER PRIMEHOST_PASSWORD PRIMEHOST_DOMAIN
+
 # start all the services
 /usr/bin/supervisord
